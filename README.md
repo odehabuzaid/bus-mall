6393d93787076ae62a150b6bda746d865dfaeb34
Odeh <abuzaid163@hotmail.com>
2021-06-21 13:02:47 +0300

    Lab12-Charts

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # bus-mall
-Lab: 11 - Get started on BusMall 
+## Lab 11
+ - Get started on BusMall 
+
+## Lab 12
+
+[![](https://data.jsdelivr.com/v1/package/npm/chart.js/badge)](https://www.jsdelivr.com/package/npm/chart.js)
---
+++ js/productschart.js
@@ -0,0 +1,32 @@
+// function productsChart() {
+//   let ctx = document.getElementById( 'Chart' ).getContext( '2d' );
+//   let myChart = new Chart( ctx, {
+//     type: 'bar',
+//     data: {
+//       labels: products,
+//       datasets: [{
+//         label: 'Clicks',
+//         data: clicks,
+//         backgroundColor:['#2F4F4F','#2F4F4F','#2F4F4F','#2F4F4F','#2F4F4F','#2F4F4F','#2F4F4F','#2F4F4F','#2F4F4F','#2F4F4F','#2F4F4F','#2F4F4F','#2F4F4F','#2F4F4F','#2F4F4F','#2F4F4F','#2F4F4F','#2F4F4F','#2F4F4F' ] ,
+//         borderWidth: 1,
+//       }
+//       , {
+//         label: 'Views',
+//         data: views,
+//         backgroundColor: ['#B8860B','#B8860B','#B8860B','#B8860B','#B8860B','#B8860B','#B8860B','#B8860B','#B8860B','#B8860B','#B8860B','#B8860B','#B8860B','#B8860B','#B8860B','#B8860B','#B8860B','#B8860B','#B8860B'],
+//         borderWidth: 1
+//       }]
+//     },
+//     options: {
+//       responsive: true,
+//       maintainAspectRatio: false,
+//       scales: {
+//         yAxes: [{
+//           ticks: {
+//             beginAtZero:true
+//           }
+//         }]
+//       }
+//     }
+//   } );
+// }
