b5d7cc4d6a26f9f2eafa26520539059306a56515
Odeh <abuzaid163@hotmail.com>
2021-07-10 07:15:38 +0300

    jsMod

--- js/app.js
+++ js/app.js
@@ -1,163 +1,132 @@
 'use strict';
+console.groupCollapsed( 'Logs.' );
+
 const fileNames = ['bag.jpg','banana.jpg','bathroom.jpg' ,'boots.jpg','breakfast.jpg',
   'bubblegum.jpg','chair.jpg' ,'cthulhu.jpg','dog-duck.jpg','dragon.jpg','pen.jpg',
   'pet-sweep.jpg','shark.jpg','sweep.png','tauntaun.jpg','unicorn.jpg','usb.gif',
   'water-can.jpg','wine-glass.jpg' ];
-let currentVIewIndex = [];
-let firstImage = document.getElementById( 'firstImg' );
-let secondImage = document.getElementById( 'secondImg' );
-let thirdImage = document.getElementById( 'thirdImg' );
-let imgContainer = document.getElementById( 'imgContainer' );
-let btnShowData = document.getElementById( 'btnShowData' );
-let btnClearData = document.getElementById( 'btnClearData' );
-let btnRestartSurvay = document.getElementById( 'btnRestartSurvay' );
 
+let currentVIewIndex = [];
 let rounds  = 25;
-let Products = function( productName, imageUrl ,clicks,viewCount ) {
+
+function Products( productName, imageUrl ,clicks,viewCount ) {
   this.productName = productName;
   this.imageUrl = `./assets/${imageUrl}`;
   this.clicks = clicks;
   this.viewCount = viewCount;
   Products.allProducts.push( this );
-};
+}
 Products.allProducts = [];
 
-function randomNumber( min, max ) {
-  return Math.floor( Math.random() * ( max - min + 1 ) + min );
-}
-function render() {
-  let leftIndex ,rightIndex ,midIndex ;
-  do {
-    midIndex = randomNumber( 0, fileNames.length - 1 );
-    leftIndex = randomNumber( 0, fileNames.length - 1 );
-    rightIndex = randomNumber( 0, fileNames.length - 1 );
-  } while( ( leftIndex === midIndex || midIndex === rightIndex ) || leftIndex === rightIndex );
-  if ( ( currentVIewIndex.includes( leftIndex ) || currentVIewIndex.includes( midIndex ) ) || currentVIewIndex.includes( rightIndex ) ) {
-    render();
-  }else{
-    firstImage.src = Products.allProducts[leftIndex].imageUrl;
-    secondImage.src = Products.allProducts[midIndex].imageUrl;
-    thirdImage.src = Products.allProducts[rightIndex].imageUrl;
-    Products.allProducts[rightIndex].viewCount++;
-    Products.allProducts[midIndex].viewCount++;
-    Products.allProducts[leftIndex].viewCount++;
-    currentVIewIndex.length = 0;
-    currentVIewIndex.push( leftIndex,midIndex,rightIndex );
-  }
-}
-imgContainer.addEventListener( 'click', clickPerformed );
-function clickPerformed( e ){
+Products.prototype.loadFromLocalStorage = () => JSON.parse( localStorage.getItem( 'products' ) ) || [];
+Products.prototype.saveToLocalStorage = ( array ) => { localStorage.setItem( 'products', JSON.stringify( array ) );};
+Products.prototype.randomNumber = ( min, max ) => Math.floor( Math.random() * ( max - min + 1 ) + min );
+
+Products.prototype.handlesImageClick = ( e ) => {
   if( rounds > 0 ){
     switch ( e.target.id ) {
     case 'firstImg':
-      Products.allProducts[currentVIewIndex[0]].clicks++; render(); rounds--;
+      Products.allProducts[currentVIewIndex[0]].clicks++; Products.prototype.displayImages(); rounds--;
       break;
     case 'secondImg':
-      Products.allProducts[currentVIewIndex[1]].clicks++; render(); rounds--;
+      Products.allProducts[currentVIewIndex[1]].clicks++; Products.prototype.displayImages(); rounds--;
       break;
     case 'thirdImg':
-      Products.allProducts[currentVIewIndex[2]].clicks++; render(); rounds--;
+      Products.allProducts[currentVIewIndex[2]].clicks++; Products.prototype.displayImages(); rounds--;
       break;
     default:
       break;
     }
-    localStorage.setItem( 'products', JSON.stringify( Products.allProducts ) );
+    Products.prototype.saveToLocalStorage( Products.allProducts );
   }else if ( rounds <= 0 ){
-    imgContainer.removeEventListener( 'click', clickPerformed );
-    btnShowData.style.display = 'inline-flex';
-    firstImage.classList.remove( 'zoom' );
-    secondImage.classList.remove( 'zoom' );
-    thirdImage.classList.remove( 'zoom' );
+    document.getElementById( 'imgContainer' ).removeEventListener( 'click', Products.prototype.handlesImageClick );
+    htmlElementsStyle();
   }
-}
+};
 
-btnShowData.style.display = 'none';
-btnClearData.style.display = 'none';
-btnRestartSurvay.style.display = 'none';
-btnShowData.addEventListener( 'click', showData );
-btnClearData.addEventListener( 'click', clearData );
-btnRestartSurvay.addEventListener( 'click', restartSurvay );
-function clearData(){
-  localStorage.clear();
-}
-function restartSurvay(){
-  location.reload();
-}
-function showData(){
+Products.prototype.displayImages = ( ) => {
+  let firstImage = document.getElementById( 'firstImg' );
+  let secondImage = document.getElementById( 'secondImg' );
+  let thirdImage = document.getElementById( 'thirdImg' );
+
+  let imagesArray = [firstImage,secondImage,thirdImage];
+  let leftIndex,midIndex,rightIndex ;
+
+  do {
+    leftIndex = Products.prototype.randomNumber( 0, fileNames.length - 1 );
+    midIndex = Products.prototype.randomNumber( 0, fileNames.length - 1 );
+    rightIndex = Products.prototype.randomNumber( 0, fileNames.length - 1 );
+  } while( ( leftIndex === midIndex || midIndex === rightIndex ) || leftIndex === rightIndex );
+
+  let newIndices = [leftIndex,midIndex ,rightIndex];
+  const checkNewIndices = () => newIndices.some( item => currentVIewIndex.includes( item ) );
+
+  if ( checkNewIndices === true ) {
+    Products.prototype.displayImages();
+  }else{
+    let count = 0;
+    for ( let i of imagesArray ) {
+      i.src = Products.allProducts[newIndices[count]].imageUrl;
+      Products.allProducts[newIndices[count]].viewCount++;
+      count++;}
+    currentVIewIndex.length = 0;
+    currentVIewIndex.push( leftIndex,midIndex,rightIndex );
+  }
+};
+
+Products.prototype.append = ( i ) =>{
   let productsUnorderedList = document.getElementById( 'productsUnorderedList' );
-  productsUnorderedList.innerHTML = '';
-  let sorted = Products.allProducts.sort( ( a, b ) => ( b.viewCount > a.viewCount ? 1 : -1 ) );
-  for ( let i = 0 ; i < sorted.length ; i++ ) {
-    let productsList = document.createElement( 'li' );
-    productsList.innerHTML = `${sorted[i].productName} : had ${sorted[i].clicks} Votes, and was seen ${sorted[i].viewCount} times.`;
-    productsUnorderedList.appendChild( productsList );
-    btnShowData.style.display = 'none';
-    btnClearData.style.display = 'inline-flex';
-    btnRestartSurvay.style.display = 'inline-flex';
-  }
-  let chartdiv = document.getElementsByClassName( 'chart' )[0];
-  chartdiv.classList.add( 'slided' );
-  productsChart();
-  localStorage.setItem( 'products', JSON.stringify( sorted ) );
-}
-function productsChart() {
-  let products = [];
-  let clicks = [];
-  let views = [];
-  for ( let i = 0; i < Products.allProducts.length; i++ ) {
-    products.push( Products.allProducts[i].productName );
-    clicks.push( Products.allProducts[i].clicks );
-    views.push( Products.allProducts[i].viewCount );
-  }
-  let ctx = document.getElementById( 'Chart' ).getContext( '2d' );
-  // eslint-disable-next-line no-undef
-  new Chart( ctx, {
-    type: 'bar',
-    data: {
-      labels: products,
-      datasets: [{
-        label: 'Clicks',
-        data: clicks,
-        backgroundColor: '#2F4F4F',
-        borderWidth: 1,
-      }
-      , {
-        label: 'Views',
-        data: views,
-        backgroundColor: '#B8860B',
-        borderWidth: 1
-      }]
-    },
-    options: {
-      responsive: true,
-      maintainAspectRatio: false,
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero:true
-          }
-        }]
-      }
-    }
-  } );
-}
+  let productsList = document.createElement( 'li' );
+  productsList.innerHTML = `${i.productName} : had ${i.clicks} Votes, and was seen ${i.viewCount} times.`;
+  productsUnorderedList.appendChild( productsList );
+  productsChart( i.productName ,i.clicks , i.viewCount );
+};
 
-function getData() {
-  let data = JSON.parse( localStorage.getItem( 'products' ) );
-  if( data ) {
-    btnClearData.style.display = 'inline-flex';
-    btnRestartSurvay.style.display = 'inline-flex';
-
-    for( let i = 0; i < data.length; i++ ) {
-      new Products( data[i].productName,( ( data[i].imageUrl ).replace( './assets/', '' ) ) , data[i].clicks, data[i].viewCount );
-    }
-    render(); console.log( Products.allProducts );
-    showData();
+window.onload = () => {
+  let data = Products.prototype.loadFromLocalStorage();
+  if( data.length > 0 ) {
+    for( let i of data ) { new Products( i.productName,( ( i.imageUrl ).replace( './assets/', '' ) ) , i.clicks, i.viewCount ); }
   }else {
-    for( let i = 0; i < fileNames.length; i++ ) {
-      new Products( fileNames[i].split( '.' )[0], fileNames[i],0,0 );
-    }
-    render();
+    for( let i = 0; i < fileNames.length; i++ ) { new Products( fileNames[i].split( '.' )[0], fileNames[i],0,0 ); }
   }
-}
-getData();
+  Products.prototype.displayImages();
+  htmlElements();
+};
+
+const htmlElementsStyle = () =>{
+  let btnShowData = document.getElementById( 'btnShowData' );
+  btnShowData.style.display = 'inline-flex';
+  let firstImage = document.getElementById( 'firstImg' );
+  let secondImage = document.getElementById( 'secondImg' );
+  let thirdImage = document.getElementById( 'thirdImg' );
+  firstImage.classList.remove( 'zoom' );
+  secondImage.classList.remove( 'zoom' );
+  thirdImage.classList.remove( 'zoom' );
+};
+
+const showData = () =>{
+  document.getElementById( 'productsUnorderedList' ).innerHTML = '';
+  products = [];
+  clicks = [];
+  views = [];
+  for ( const i of Products.allProducts ) {
+    Products.prototype.append( i );
+  }
+
+};
+
+const htmlElements = () => {
+  let btnShowData = document.getElementById( 'btnShowData' );
+  let btnClearData = document.getElementById( 'btnClearData' );
+  let btnRestartSurvay = document.getElementById( 'btnRestartSurvay' );
+  btnShowData.style.display = 'none';
+  btnClearData.style.display = 'none';
+  btnRestartSurvay.style.display = 'none';
+  btnShowData.addEventListener( 'click', showData );
+  btnClearData.addEventListener( 'click',() => {localStorage.removeItem( 'products' );location.reload();} );
+  btnRestartSurvay.addEventListener( 'click', () => { location.reload();} );
+  document.getElementById( 'imgContainer' )
+    .addEventListener( 'click', Products.prototype.handlesImageClick );
+};
+
+// Odeh abuzaid
--- js/productschart.js
+++ js/productschart.js
@@ -1,32 +1,48 @@
-// function productsChart() {
-//   let ctx = document.getElementById( 'Chart' ).getContext( '2d' );
-//   let myChart = new Chart( ctx, {
-//     type: 'bar',
-//     data: {
-//       labels: products,
-//       datasets: [{
-//         label: 'Clicks',
-//         data: clicks,
-//         backgroundColor:['#2F4F4F','#2F4F4F','#2F4F4F','#2F4F4F','#2F4F4F','#2F4F4F','#2F4F4F','#2F4F4F','#2F4F4F','#2F4F4F','#2F4F4F','#2F4F4F','#2F4F4F','#2F4F4F','#2F4F4F','#2F4F4F','#2F4F4F','#2F4F4F','#2F4F4F' ] ,
-//         borderWidth: 1,
-//       }
-//       , {
-//         label: 'Views',
-//         data: views,
-//         backgroundColor: ['#B8860B','#B8860B','#B8860B','#B8860B','#B8860B','#B8860B','#B8860B','#B8860B','#B8860B','#B8860B','#B8860B','#B8860B','#B8860B','#B8860B','#B8860B','#B8860B','#B8860B','#B8860B','#B8860B'],
-//         borderWidth: 1
-//       }]
-//     },
-//     options: {
-//       responsive: true,
-//       maintainAspectRatio: false,
-//       scales: {
-//         yAxes: [{
-//           ticks: {
-//             beginAtZero:true
-//           }
-//         }]
-//       }
-//     }
-//   } );
-// }
+let products = [];
+let clicks = [];
+let views = [];
+
+const productsChart = ( product,click,view ) => {
+  let chartdiv = document.getElementsByClassName( 'chart' )[0];
+  chartdiv.classList.add( 'slided' );
+  products.push( product );
+  clicks.push( click );
+  views.push( view );
+
+  let ctx = document.getElementById( 'Chart' ).getContext( '2d' );
+  // eslint-disable-next-line no-undef
+  new Chart( ctx, {
+    type: 'bar',
+    data: {
+      labels: products,
+      datasets: [{
+        label: 'Clicks',
+        data: clicks,
+        backgroundColor: '#2F4F4F',
+        borderWidth: 1,
+      }
+      , {
+        label: 'Views',
+        data: views,
+        backgroundColor: '#B8860B',
+        borderWidth: 1
+      }]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero:true
+          }
+        }]
+      }
+    }
+  } );
+
+
+  btnShowData.style.display = 'none';
+  btnClearData.style.display = 'inline-flex';
+  btnRestartSurvay.style.display = 'inline-flex';
+};
